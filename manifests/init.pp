6ca7ef69f357814862fa1249478c7d466efa5c80
ricky <rickyp72@googmail.com>
2014-02-05 13:06:13 +0000

    init commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,17 @@
 # This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class myspotify {
+  $install_file = '/Applications/Install\ Spotify.app'
+
+  package { 'Spotify':
+    provider => 'compressed_app',
+    source   => 'http://download.spotify.com/SpotifyInstaller.zip',
+  }
+  ~> exec { 'Install Spotify':
+    command     => "/usr/bin/open ${install_file}",
+    logoutput   => 'on_failure',
+    refreshonly => true,
+  }
+  -> file { $install_file:
+    ensure => 'absent',
+  }
 }
